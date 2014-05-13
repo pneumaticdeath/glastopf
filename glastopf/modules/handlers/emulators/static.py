a90fda0cfabb755f294bdf556139dc0453825211
Mitch Patenaude <patenaude@gmail.com>
2014-05-13 09:20:28 -0700

    Changed copywrite line in modeules created by me.

--- glastopf/modules/handlers/emulators/static.py
+++ glastopf/modules/handlers/emulators/static.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012  Phani Vadrevu
+# Copyright (C) 2014 Mitch Patenaude
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
